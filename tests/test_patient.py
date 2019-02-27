cbfe211932d3427dc69709c5bc67878a52ae1d5f
Felix Eichenberger <felix.eichenberger@gyrotools.com>
2019-02-27 15:30:40 +0100

    Fix tests

--- tests/test_patient.py
+++ tests/test_patient.py
@@ -33,6 +33,7 @@ class TestPatient:
         connection = BasicConnection('http://localhost', 'test', 'test')
         http_client = FakeClient(connection)
         p = Patient(http_client)
+        assert p
 
     def test_get(self):
         connection = BasicConnection('http://localhost', 'test', 'test')
@@ -66,59 +67,66 @@ class TestPatient:
     def test_get_list(self):
         connection = BasicConnection('http://localhost', 'test', 'test')
 
-        get_data = [
-            {
-                "id": 1,
-                "permissions": {
-                    "write": True,
-                    "read": True
+        get_data = {
+            "count": 6,
+            "limit": 10,
+            "offset": 0,
+            "next": None,
+            "previous": None,
+            "results": [
+                {
+                    "id": 1,
+                    "permissions": {
+                        "write": True,
+                        "read": True
+                    },
+                    "most_recent_exam_date": "2016-04-21T13:43:13Z",
+                    "created_date": "2016-10-11T12:46:13Z",
+                    "modified_date": "2018-11-22T06:48:24Z",
+                    "name": "Daniel Smith",
+                    "patient_id": "HOSP2",
+                    "birth_date": "1980-12-05",
+                    "sex": "m",
+                    "weight": 72,
+                    "anonymous": False,
+                    "creator": None
                 },
-                "most_recent_exam_date": "2016-04-21T13:43:13Z",
-                "created_date": "2016-10-11T12:46:13Z",
-                "modified_date": "2018-11-22T06:48:24Z",
-                "name": "Daniel Smith",
-                "patient_id": "HOSP2",
-                "birth_date": "1980-12-05",
-                "sex": "m",
-                "weight": 72,
-                "anonymous": False,
-                "creator": None
-            },
-            {
-                "id": 2,
-                "permissions": {
-                    "write": True,
-                    "read": True
+                {
+                    "id": 2,
+                    "permissions": {
+                        "write": True,
+                        "read": True
+                    },
+                    "most_recent_exam_date": "2016-04-21T10:58:16Z",
+                    "created_date": "2016-10-11T12:57:32Z",
+                    "modified_date": "2018-04-03T15:09:33Z",
+                    "name": "Marcel Hoppe",
+                    "patient_id": "HOSP1",
+                    "birth_date": "1974-03-07",
+                    "sex": "m",
+                    "weight": 80,
+                    "anonymous": False,
+                    "creator": None
                 },
-                "most_recent_exam_date": "2016-04-21T10:58:16Z",
-                "created_date": "2016-10-11T12:57:32Z",
-                "modified_date": "2018-04-03T15:09:33Z",
-                "name": "Marcel Hoppe",
-                "patient_id": "HOSP1",
-                "birth_date": "1974-03-07",
-                "sex": "m",
-                "weight": 80,
-                "anonymous": False,
-                "creator": None
-            },
-            {
-                "id": 3,
-                "permissions": {
-                    "write": True,
-                    "read": True
+                {
+                    "id": 3,
+                    "permissions": {
+                        "write": True,
+                        "read": True
+                    },
+                    "most_recent_exam_date": "2016-04-21T12:26:11Z",
+                    "created_date": "2016-10-11T12:59:03Z",
+                    "modified_date": "2016-10-28T12:31:37Z",
+                    "name": "Stefan Meier",
+                    "patient_id": "HOSP4",
+                    "birth_date": "1976-12-08",
+                    "sex": "m",
+                    "weight": 71,
+                    "anonymous": False,
+                    "creator": None
                 },
-                "most_recent_exam_date": "2016-04-21T12:26:11Z",
-                "created_date": "2016-10-11T12:59:03Z",
-                "modified_date": "2016-10-28T12:31:37Z",
-                "name": "Stefan Meier",
-                "patient_id": "HOSP4",
-                "birth_date": "1976-12-08",
-                "sex": "m",
-                "weight": 71,
-                "anonymous": False,
-                "creator": None
-            },
-        ]
+            ]
+        }
 
         http_client = FakeClient(connection)
 
