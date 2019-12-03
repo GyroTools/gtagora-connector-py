185923d5131e70d33091f45a42ca353277ce56ca
Martin <piwo76@gmail.com>
2019-12-03 17:34:17 +0100

    Version 0.1.6

--- Dockerfile
+++ Dockerfile
@@ -1,2 +1,2 @@
 FROM python:3.7
-RUN pip install gtagora-connector --upgrade
+RUN pip install gtagora-connector --upgrade --no-cache-dir
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="gtagora-connector",
-    version="0.1.5",
+    version="0.1.6",
     author="Martin Bührer, Felix Eichenberger",
     author_email="info@gyrotools.com",
     description="The Agora Connector for Python",
