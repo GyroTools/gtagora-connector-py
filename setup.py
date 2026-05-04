aee9488a513d5b12f5ae29d16b189e8f75a7a5a1
martin <martin.buehrer@gyrotools.com>
2026-05-04 11:14:52 +0200

    version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="gtagora-connector",
-    version="1.7.8",
+    version="1.7.9",
     author="Martin Bührer, Felix Eichenberger",
     author_email="info@gyrotools.com",
     description="The Agora Connector for Python",
