95773c733a23839e3d325f51d0e6f1c6bb7357bc
martin <martin.buehrer@gyrotools.com>
2026-09-15 12:53:44 +0200

    added a new extended docker container for nifti writing

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="gtagora-connector",
-    version="1.8.0",
+    version="1.8.1",
     author="Martin Bührer, Felix Eichenberger",
     author_email="info@gyrotools.com",
     description="The Agora Connector for Python",
